0e756ab422fb993fe304f48090d0d4d2807e0a2f
ahmedhussein74 <ahmedhussein.2072@gmail.com>
2022-07-02 10:22:26 +0200

    commit

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React , {Component} from 'react'
+import AOS from 'aos'
 
-function Info (){
+class Info extends Component (){
+  componentDidMount() {
+    AOS.init({
+      duration : 2000
+    });
+  }
+  render(){
     return(
-      <section className='text-center'>
-      </section>
+      <section className='info text-white w-75 mx-auto p-3 rounded bg-dark' data-aos={"fade-left"}>
+      <div>Name</div><div>: Ahmed Hussein</div>
+      <div>Age</div><div>: 21 Year</div>
+      <div>Job</div><div>: Front end</div>
+      <div>Phone</div><div>: 01102466073</div>
+      <div>Address</div><div>: Giza - 6<sup>th</sup> October</div>
+    </section>
     )
+  }
 }
 
 export default Info
